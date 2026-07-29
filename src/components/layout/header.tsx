aa2470e96eeaa28c3f7c552375e4f69c06302c1c
tack0sim <talhamjd@gmail.com>
2026-07-29 11:33:48 +0200

    chore: cleanup header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -11,15 +11,14 @@ export async function Header() {
 
   return (
     <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-      <div className="flex w-full items-center justify-between gap-2 px-4">
-        {isAuthenticated ? (
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator
-              className="mr-2 data-vertical:h-4 data-vertical:self-auto"
-              orientation="vertical"
-            />
-            {/* <Breadcrumb>
+      {isAuthenticated ? (
+        <div className="flex items-center gap-2 px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator
+            className="mr-2 data-vertical:h-4 data-vertical:self-auto"
+            orientation="vertical"
+          />
+          {/* <Breadcrumb>
               <BreadcrumbList>
                 <BreadcrumbItem className="hidden md:block">
                   <BreadcrumbLink href="#">
@@ -32,23 +31,17 @@ export async function Header() {
                 </BreadcrumbItem>
               </BreadcrumbList>
             </Breadcrumb> */}
-            <span className="font-normal text-foreground text-xs">
-              Welcome to Family Assist
-            </span>
-          </div>
-        ) : (
-          <div className="flex items-center gap-2 px-4">
-            {/* Global navigation for unauthenticated users */}
-          </div>
-        )}
-        <div>
-          {!isAuthenticated && (
-            <Link href="/auth/login">
-              <Button>Sign In</Button>
-            </Link>
-          )}
+          <span className="font-normal text-foreground text-xs">
+            Welcome to Family Assist
+          </span>
         </div>
-      </div>
+      ) : (
+        <div className="flex w-full items-center px-4">
+          <Link className="ml-auto" href="/auth/login">
+            <Button>Sign In</Button>
+          </Link>
+        </div>
+      )}
     </header>
   );
 }
