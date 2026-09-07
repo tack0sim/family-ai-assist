1283f24c53eb6c66886e50d61040e8a01b78869a
tack0sim <talhamjd@gmail.com>
2026-09-07 18:21:52 +0200

    chore: set app version env variable

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,5 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const APP_VERSION = process.env.NEXT_PUBLIC_APP_VERSION ?? "unknown";
